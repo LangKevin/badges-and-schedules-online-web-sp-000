bd313dde834f8714a02322e8fa6f06d6614b3f17
kevin lang <langkevina@gmail.com>
2019-05-21 00:06:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -35,7 +35,7 @@ end
 
 def printer(attendees)
   # return "Hello, my name is #{attendees[0]}."
-  attendees.each do |item|
-    puts "Hello, my name is #{item}."
+  batch_badge_creator(attendees).each do |item|
+    puts "#{item}."
   end
 end
